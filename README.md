4bf3995589b6240c34aa2c0501e4a8a2a0007123
Bradinator64 <bradaz64@gmail.com>
2021-05-19 10:43:49 +0100

    Initialise Player and Item classes
    Initialise the Player and Item classes, the idea being that each item will have a list of equipment slots it can be equipped to.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RPG Player Test
- Testing creating a player object for a test-based rpg
+ Testing creating a player object for a test-based rpg using Python 3.
+This is primarily testing creating an inventory system for the player,
+as well as the ability to equip and unequip items.
--- playerTest1.py
+++ playerTest1.py
@@ -8,3 +8,20 @@
 import os
 
 """classes"""
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = {}
+        self.equipmentSlots = {
+            "armour" : null,
+            "mainHand" : null,
+            "offhand" : null
+        }
+
+
+class Item():
+    def __init__(self, name, slots=[]):
+        self.name = name
+        self.slots = slots
+
