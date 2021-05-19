53e51f4023495be34669829ef1aca4bfc7bd5fbc
Bradinator64 <bradaz64@gmail.com>
2021-05-19 12:37:50 +0100

    Add ability for player objects to equip items in their inventory
    Player objects can now equip items from their inventory, if they have an item already equipped, it will be placed back in the player object's inventory

--- playerTest1.py
+++ playerTest1.py
@@ -21,29 +21,56 @@ class Player:
 
     def addToInventory(self, item):
         if isinstance(item, Item):
-            self.inventory[item.name] = item
+            self.inventory.update({item.name : item})
+            #self.inventory[item.name] = item
             print(str(item.name) + " added to inventory")
         else:
             print("item cannot be added to inventory")
 
+    #checks whether the object's inventory contains the
+    #object specified in the parameter.  Returns None if
+    #the object is not in the Player object's inventory,
+    #otherwise it returns the object.
     def checkInventory(self, item):
         ItemFound = None
         if item in self.inventory.values():
             return item
         else:
             return None
+            
+    def equipItem(self, item):
+        itemSlot = self.equipmentSlots[item.slot]
+        if self.checkInventory(item):
+            if itemSlot != None:
+                self.addToInventory(itemSlot)
+            self.equipmentSlots[item.slot] = item
+            del self.inventory[item.name]
+        else:
+            print("Item not in player inventory")
+
 
 class Item:
-    def __init__(self, name, slots=[]):
+    def __init__(self, name, slot):
         self.name = name
-        self.slots = slots
+        self.slot = slot
 
 testPlayer = Player("testPlayer")
-Sword = Item("Sword", ["mH"])
-Spear = Item("Spear", ["mH"])
+Sword = Item("Sword", "mH")
+Spear = Item("Spear", "mH")
 
 testPlayer.addToInventory(Sword)
-testPlayer.addToInventory("test")
-print(testPlayer.checkInventory(Spear))
+#testPlayer.addToInventory("test")
+#print(testPlayer.checkInventory(Spear))
+#print(testPlayer.checkInventory(Sword))
 
+testPlayer.equipItem(Sword)
+print("Player Inventory:")
 print(testPlayer.inventory)
+print("Player Equipment:")
+print(testPlayer.equipmentSlots)
+testPlayer.addToInventory(Spear)
+testPlayer.equipItem(Spear)
+print("Player Inventory after spear:")
+print(testPlayer.inventory)
+print("Player equipment after spear:")
+print(testPlayer.equipmentSlots)
